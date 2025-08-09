ad9bcd453240678906b1dd39d157c0d58d9aff44
dennis-ny <denn.ny173@gmail.com>
2025-08-09 15:38:12 +0700

    feat: implement authentication controller with register, login, and logout functionalities

--- src/routes/public-api.ts
+++ src/routes/public-api.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middleware/auth-middleware.js";
-import userController from "../controller/user-controller.js";
+import authController from "../controller/auth-controller.js";
 const publicRouter = express.Router();
 
 // auth
-publicRouter.post("/api/auth/register", userController.register);
-publicRouter.post("/api/auth/login", userController.login);
-publicRouter.post("/api/auth/logout", authMiddleware, userController.logout);
+publicRouter.post("/api/auth/register", authController.register);
+publicRouter.post("/api/auth/login", authController.login);
+publicRouter.post("/api/auth/logout", authMiddleware, authController.logout);
 
 export { publicRouter };
